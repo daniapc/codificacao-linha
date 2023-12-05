18a431b2735a150df0036fe49831ec8a45c48e2d
daniapc <daniel.castro0811@gmail.com>
2023-12-05 13:08:02 -0300

    Interface Gráfica Emissor

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ https://www.youtube.com/watch?v=Et0fYeA2XxY
 https://www.youtube.com/watch?v=sUzM-vIC-s4
 https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-strings-in-python/
 https://www.geeksforgeeks.org/python-program-to-convert-ascii-to-binary/
+https://www.dropbox.com/scl/fo/w660w2rnhids0p4ymlvah/h/modelo%20e%20normas%20para%20trabalho/cod_linha.pdf?rlkey=zqksqfrai8ckkte3bmgz2bx91&dl=0
+https://www.youtube.com/watch?v=w_-d4w1k87U
--- emissor/TelaEmissor.py
+++ emissor/TelaEmissor.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 from cryptography.fernet import Fernet
+import matplotlib.pyplot as plt
 
 CHAVE = "v3oK2y7pLpkodK374oVZ_we6cNp4qseOwfOCcSOq1mg="
 
@@ -29,11 +30,84 @@ class TelaEmissor:
 
             binaria = bin(int.from_bytes(criptografada.encode(), "big"))[2:]
 
+            hdb3 = self.hdb3(binaria)
+
             print(f'Mensagem Escrita: {escrita}')
             print(f'Mensagem Criptografada: {criptografada}')
             print(f'Mensagem Em Binário: {binaria}')
+            print(f'Mensagem Em HDB3: {hdb3}')
+            
+            self.fazer_grafico(binaria, "Gráfico Binário")
+            self.fazer_grafico(hdb3, "Gráfico HDB3")
 
             self.button, self.values = self.janela.Read()
+            
+
+    def hdb3(self, binario):
+        substituido = list(binario)
+        
+        index = 0
+        
+        pulsos_high = 0
+
+        size = len(binario)-3
+
+        while index < size:
+
+            if binario[index] == '1':
+                pulsos_high += 1
+
+            sequencia = binario[index:index+4]
+
+            if sequencia == '0000':
+                if pulsos_high%2 == 0:
+                    substituido[index]   = 'B'
+                    substituido[index+1] = '0'
+                    substituido[index+2] = '0'
+                    substituido[index+3] = 'V'
+                else:
+                    substituido[index]   = '0'
+                    substituido[index+1] = '0'
+                    substituido[index+2] = '0'
+                    substituido[index+3] = 'V'
+
+                pulsos_high = 0
+                index += 3
+            
+            index += 1
+        
+        
+        return ''.join(substituido)
+
+    def fazer_grafico(self, binario, titulo):
+        tam = len(binario)
+
+        binario = binario.replace('B', '1').replace('V', '1')
+
+        fig = plt.figure(titulo)
+
+        plt.plot((0, 0+1), (int(binario[0]), int(binario[0])), color = 'b')
+
+        for i in range(1, tam -1):
+            value = int(binario[i])
+
+            if binario[i - 1] == '0':
+                plt.plot((i, i), (0, value), color = 'b')
+            plt.plot((i, i+1), (value, value), color = 'b')
+            if binario[i + 1] == '0':
+                plt.plot((i+1, i+1), (value, 0), color = 'b')
+        
+        if binario[tam - 1] == '0':
+            plt.plot((tam-1, tam-1), (0, int(binario[tam-1])), color = 'b')
+        plt.plot((tam-1, tam-1), (int(binario[tam-1]), int(binario[tam-1])), color = 'b')
+
+
+        ax = plt.gca() 
+        ax.set_ylim([0, 2])
+        plt.xlabel("X")
+        plt.ylabel("y")
+        plt.title(titulo) 
+        plt.show()
 
 
 tela = TelaEmissor()
